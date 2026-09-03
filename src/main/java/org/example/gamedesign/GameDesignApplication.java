c0437e36ff2ca391b558d38ea70846b6b31245d5
solo <shiyatao_5@163.com>
2026-09-03 21:41:40 +0800

    init: 完成命令模式基础架构与核心测试

---
+++ src/main/java/org/example/gamedesign/GameDesignApplication.java
@@ -0,0 +1,13 @@
+package org.example.gamedesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameDesignApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/org/example/gamedesign/GameDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gamedesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameDesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
